b02358348684b97dbec5c23e78d0ef383835641a
wangxuefeng <wangxuefeng605@gmail.com>
2026-08-06 22:00:47 +0800

    fix learning package export for feedback engine

--- backend/ai_selector/core/learning/__init__.py
+++ backend/ai_selector/core/learning/__init__.py
@@ -1,41 +1,5 @@
-from .feedback_engine import FeedbackEngine
-from .optimizer import LearningOptimizer
-from .runtime import LearningRuntime
-from .feedback import evaluate_prediction
-from .scanner_learning_hook import ScannerLearningHook
-from .ranking_learning_hook import RankingLearningHook
-from .learning_pipeline import LearningPipeline
-from .learning_runtime_orchestrator import (
-    LearningRuntimeOrchestrator
-)
-from .feedback_analyzer import FeedbackAnalyzer
-
-
-__all__ = [
-    "FeedbackAnalyzer",
-]
-
-from .weight_provider import (
-    WeightProvider,
-    LearningWeightProvider,
-    inject_weights,
-    inject_learning_weight,
-)
-
-
-__all__ = [
-    "FeedbackEngine",
-    "LearningOptimizer",
-    "LearningRuntime",
-
-    "FeedbackAnalyzer",
-    "ScannerLearningHook",
-
-    "WeightProvider",
-    "LearningWeightProvider",
-
-    "inject_weights",
-    "inject_learning_weight",
-
-    "evaluate_prediction",
-]
+from .portfolio_feedback import *
+from .performance_evaluator import *
+from .learning_signal import *
+from .weight_adapter import *
+from .feedback_engine import FeedbackLearningEngine
